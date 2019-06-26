7a3fcbae625c897099ade1ac9803c233ab38fe4c
MIGUEL ANGEL MUENALA QUINGA <miguel.muenala@epn.edu.ec>
2019-06-26 10:57:37 -0500

    Cambios
    Cambios

--- sharpDevelop/PrjCrearSolucion/PrjCrearSolucion/MainForm.Designer.cs
+++ sharpDevelop/PrjCrearSolucion/PrjCrearSolucion/MainForm.Designer.cs
@@ -36,12 +36,17 @@ namespace PrjCrearSolucion
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.SuspendLayout();
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "PrjCrearSolucion";
+			this.ClientSize = new System.Drawing.Size(636, 404);
 			this.Name = "MainForm";
+			this.Text = "PrjCrearSolucion";
+			this.ResumeLayout(false);
+
 		}
 	}
 }
