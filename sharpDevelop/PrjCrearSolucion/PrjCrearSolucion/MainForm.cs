8d71ef90bd52c562389d5ffa3ea8906846250d8d
MIGUEL ANGEL MUENALA QUINGA <miguel.muenala@epn.edu.ec>
2019-06-26 10:24:45 -0500

    Crear soluc en SharpDevelop
    Crear soluc en SharpDevelop

---
+++ sharpDevelop/PrjCrearSolucion/PrjCrearSolucion/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: USRGAM
+ * Date: 26/06/2019
+ * Time: 10:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PrjCrearSolucion
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
